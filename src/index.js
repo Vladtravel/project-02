9b7c66ce8a7da6913dc587f06c1207ca93cb045b
Denis-us <75166604+Denis-us@users.noreply.github.com>
2021-06-18 00:35:25 +0300

    Сделал модалку

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './sass/main.scss';
-<<<<<<< HEAD
-import './JS/modal.js'
-=======
+import './JS/modal.js';
 
-// import './JS/apiServicePop.js';
->>>>>>> 3c509898330645589a80a8c9cd15aaebd948807f
+import './JS/apiServicePop.js';
+
