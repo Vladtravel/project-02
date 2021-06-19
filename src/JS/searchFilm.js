a53f332ee70d52d6d285fde5fd708e097665ba83
Oleksii Protasov <oleksi.protasov@gmail.com>
2021-06-19 21:24:39 +0300

    Update searchFilm.js

--- src/JS/searchFilm.js
+++ src/JS/searchFilm.js
@@ -1,9 +1,35 @@
 import VideoApiService from './apiServese';
 import cardImeges from '../templates/templates.hbs';
 
+const galleryList = document.querySelector('.gallery-list');
 const formInput = document.querySelector('.header-form');
-const filmApiServ = new VideoApiService();
+const filmApiService = new VideoApiService();
 
 formInput.addEventListener('submit', searchingFilm);
+
+function searchingFilm(e) {
+    e.preventDefault();
+  
+    const form = e.currentTarget;
+    const input = form.elements.query;
+  
+    // clearListItems();
+  
+    // filmApiService.resetPage();
+    filmApiService.searchQuerry = input.value;
+  
+    filmApiService.fetchVideo().then(hits => {
+        buildListItemsTemplate(hits);
+      console.log()
+    });
+    input.value = '';
+
+  }
+  
+  
+  function buildListItemsTemplate(items) {
+    return galleryList.insertAdjacentHTML('beforeend', filmsTpl(films));
+  }
+
 
 
