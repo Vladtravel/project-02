075bc7c69c3d14166cccefc8ac1ab2b149660e9f
dbeshchuk <dmytro.beshchuk@protonmail.com>
2021-06-19 19:48:01 +0300

    Update myLibrary.js

--- src/JS/myLibrary.js
+++ src/JS/myLibrary.js
@@ -1,3 +1,4 @@
+// 1. Импорты
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = '612ad9e57f61526bfd55d457eca2466c';
@@ -6,6 +7,8 @@ const testWatchedID = [508943, 84958, 602063, 581726]
 const testQueueID = [581644, 96677]
 
 import filmsTpl from '../templates/films-gallery-markup.hbs';
+
+// 2. Тестовый фетч
 
 function testFetch(id) {
   return fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`)
