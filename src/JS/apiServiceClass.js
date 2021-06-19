531c18d3bba7043806a6e6e5c41de6bc9857c05a
Konstantin Halkin <constantingal@gmail.com>
2021-06-19 18:21:36 +0300

    показівает подгрузку популярных фильмов на главную страницу

---
+++ src/JS/apiServiceClass.js
@@ -0,0 +1,46 @@
+const API_KEY = '612ad9e57f61526bfd55d457eca2466c';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export default class NewApiServiceTrend {
+  constructor() {
+    this.page = 1;
+  }
+
+  fetchFilms() {
+    const url = `${BASE_URL}/trending/all/day?api_key=${API_KEY}&${this.page}`;
+    return fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(({ results }) => {
+        return results;
+      });
+  }
+
+  fetchGenres() {
+    const url = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        return data.genres;
+      });
+  }
+
+  insertGenresToMovieObj() {
+    return this.fetchFilms().then(data => {
+      return this.fetchGenres().then(genresList => {
+        return data.map(movie => ({
+          ...movie,
+          release_date: movie.release_date ? movie.release_date.slice(0, 4) : '',
+          first_air_date: movie.first_air_date ? movie.first_air_date.slice(0, 4) : '',
+          genres: movie.genre_ids
+            ? movie.genre_ids
+                .map(id => genresList.filter(el => el.id === id))
+                .slice(0, 2)
+                .flat()
+            : 'watch the movie and decide',
+        }));
+      });
+    });
+  }
+}
--- src/JS/apiServicePop.js
+++ src/JS/apiServicePop.js
@@ -1,21 +1,20 @@
 const API_KEY = '612ad9e57f61526bfd55d457eca2466c';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+import NewApiServiceTrend from './apiServiceClass';
 
 import filmsTpl from '../templates/films-gallery-markup.hbs';
 
 const galleryList = document.querySelector('.gallery-list');
 
-function fetchFilms() {
-  fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(films => {
-      filmsMarkup(films);
-    });
+const newApiServiceTrend = new NewApiServiceTrend();
+
+render();
+
+function filmsMarkup(results) {
+  galleryList.insertAdjacentHTML('beforeend', filmsTpl(results));
 }
 
-function filmsMarkup(films) {
-  galleryList.insertAdjacentHTML('beforeend', filmsTpl(films));
+function render() {
+  newApiServiceTrend.insertGenresToMovieObj().then(filmsMarkup);
 }
-
-fetchFilms();
