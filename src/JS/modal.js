c30baecd8bd0e98878424cafd426a91d2d8a2ed3
Denis-us <75166604+Denis-us@users.noreply.github.com>
2021-06-21 12:38:46 +0300

    разбираюсь с фетчем по id для модалки

--- src/JS/modal.js
+++ src/JS/modal.js
@@ -1,30 +1,92 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    backdrop: document.querySelector('[data-backdrop]')
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.backdrop.classList.toggle('is-hidden');
-  }
-})();
+import filmMarkup from '../templates/one-film-markup.hbs'
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
 
-function fetchFilmsCard() {
-  fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`)
-    .then(response => {
-      return response.json();
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     backdrop: document.querySelector('[data-backdrop]')
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.backdrop.classList.toggle('is-hidden');
+//   }
+// })();
+
+const API_KEY = '612ad9e57f61526bfd55d457eca2466c';
+
+const openOneFilm = document.querySelector('.gallery-list');
+
+console.log(openOneFilm)
+
+openOneFilm.addEventListener('click', openModal);
+
+function fetchFilmsCardId(movie_id){
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}`
+  return fetch(url)
+  .then(response => response.json)
+  .then(data => console.log(data))
+  // Ошибка тут! Нужно получить доступ к id того фильма на который кликаешь
+      
+}
+
+function openModal(e) {
+  e.preventDefault();
+
+  fetchFilmsCardId(e.target.dataset.id)
+    .then(data => {
+      if (e.target.nodeName !== 'IMG') return;
+
+      const markup = filmMarkup(data);
+      const modal = basicLightbox.create(markup);
+
+      modal.show();
+
+      const closeBtn = document.querySelector('.modal-close');
+      closeBtn.addEventListener('click', closeModal);
+
+      window.addEventListener('keydown', closeModalHandler);
+
+      function closeModalHandler(e) {
+        if (e.code === 'Escape') {
+          modal.close();
+          window.removeEventListener('keydown', closeModalHandler);
+        }
+      }
+
+      function closeModal(e) {
+        modal.close();
+        window.removeEventListener('keydown', closeModalHandler);
+      }
+
+      //new Function
+      initStorageBtns();
     })
-    .then(film => {
-      filmCardMarkup(film);
+    .then(data => {})
+    .catch(error => {
+      console.log('oops!');
     });
 }
 
-// https://developers.themoviedb.org/3/movies/get-movie-details
+
+
+// console.log(fetchFilmsCardId())
+
+
+
+
+
+
+// function filmCardMarkup(film) {
+//   filmMarkup.insertAdjacentHTML('beforeend', filmMarkup(film));
+// }
+
+
 
 
 
