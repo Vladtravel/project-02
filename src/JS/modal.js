58f5a6ef04bbc038820daa78b0272bc64eb1e01c
Vladyslav <vladtravel2015@gmail.com>
2021-06-23 08:02:54 +0300

    правка модалки

--- src/JS/modal.js
+++ src/JS/modal.js
@@ -1,20 +1,21 @@
 import filmMarkup from '../templates/one-film-markup.hbs';
 import NewModalService from './apiModal';
 
-const refs = {
-  openModalBtn: document.querySelector('[data-modal-open]'),
-  closeModalBtn: document.querySelector('[data-modal-close]'),
-  backdrop: document.querySelector('[data-backdrop]'),
-  containerModal: document.querySelector('.container-card'),
-};
+// const refs = {
+//   openModalBtn: document.querySelector('[data-modal-open]'),
+//   closeModalBtn: document.querySelector('[data-modal-close]'),
+//   backdrop: document.querySelector('[data-backdrop]'),
+//   ,
+// };
 
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
+// refs.openModalBtn.addEventListener('click', toggleModal);
+// refs.closeModalBtn.addEventListener('click', toggleModal);
 
-function toggleModal() {
-  refs.backdrop.classList.toggle('is-hidden');
-}
+// function toggleModal() {
+//   refs.backdrop.classList.toggle('is-hidden');
+// }
 
+const containerModal = document.querySelector('.container-card');
 const openOneFilm = document.querySelector('.gallery-list');
 
 openOneFilm.addEventListener('click', openModal);
@@ -22,15 +23,25 @@ openOneFilm.addEventListener('click', openModal);
 const ApiModal = new NewModalService();
 
 function openModal(e) {
-  e.preventDefault();
   console.log(e.target.dataset.id);
   ApiModal.query = e.target.dataset.id;
 
-  toggleModal();
+  // toggleModal();
+
+  // function some() {
+  //   const closeBtn = document.querySelector('.modal-close')
+  //   closeBtn.addEventListener('click', closeModal)
+
+  //   function closeModal(e) {
+  //     console.log(some)
+  //   }
+  // }
+
+  //
 
   ApiModal.fetchImage().then(imageMarkup);
 }
 
 function imageMarkup(data) {
-  refs.containerModal.insertAdjacentHTML('beforeend', filmMarkup(data));
+  containerModal.insertAdjacentHTML('beforeend', filmMarkup(data));
 }
