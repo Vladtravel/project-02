47ca98ae7827e8f0dd39e557f7a48b5edc965b72
Denis-us <75166604+Denis-us@users.noreply.github.com>
2021-06-25 00:14:03 +0300

    Дописал адаптивку

--- src/JS/modal.js
+++ src/JS/modal.js
@@ -2,8 +2,9 @@ import filmMarkup from '../templates/one-film-markup.hbs';
 import NewModalService from './apiModal';
 
 const containerModal = document.querySelector('.container-card');
-
+const bodyEl = document.querySelector('body')
 const openOneFilm = document.querySelector('.gallery-list');
+
 openOneFilm.addEventListener('click', openModal);
 
 const ApiModal = new NewModalService();
@@ -15,6 +16,8 @@ function openModal(e) {
   }
   ApiModal.query = e.target.dataset.id;
 
+  bodyEl.classList.add('remove-scroll');
+
   ApiModal.fetchImage().then(imageMarkup);
 }
 
@@ -23,6 +26,7 @@ function imageMarkup(data) {
   containerModal.insertAdjacentHTML('beforeend', filmMarkup(data));
   const modal = document.querySelector('[data-modal]');
   modal.classList.remove('is-hidden');
+  modal.classList.add('modal-scroll');
 
   const closeModalBtn = document.querySelector('.close-modal');
   closeModalBtn.addEventListener('click', toggleModal);
@@ -30,12 +34,22 @@ function imageMarkup(data) {
   const backdropEl = document.querySelector('.backdrop');
   backdropEl.addEventListener('click', toggleModal);
 
-  libraryButtons(data)
+  window.addEventListener('keydown', closeModalHandler);
+
+  libraryButtons(data);
+}
+
+function closeModalHandler(e) {
+  if (e.code === 'Escape') {
+    toggleModal();
+  }
 }
 
 function toggleModal() {
   const modal = document.querySelector('[data-modal]');
+
   modal.classList.add('is-hidden');
+  bodyEl.classList.remove('remove-scroll');
 }
 
 // Реализация кнопок для MyLibrary
