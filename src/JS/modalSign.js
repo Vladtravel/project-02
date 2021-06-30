15d07916b0df575111620b38dbd5b3049068343d
Vladyslav <vladtravel2015@gmail.com>
2021-06-30 09:38:06 +0300

    ок

--- src/JS/modalSign.js
+++ src/JS/modalSign.js
@@ -1,12 +1,12 @@
 import getRefs from './getRefs';
 
 const refs = getRefs();
-refs.btnSign.addEventListener('click', openModalSign)
-refs.btnCloseSign.addEventListener('click', closeByBtn)
+refs.btnSign.addEventListener('click', openModalSign);
+refs.btnCloseSign.addEventListener('click', closeByBtn);
 
 function openModalSign() {
-    refs.backdropSign.classList.remove('is-hidden')
-    refs.body.classList.add('remove-scroll')
+  refs.backdropSign.classList.remove('is-hidden');
+  refs.body.classList.add('remove-scroll');
 }
 
 function closeByBtn() {
