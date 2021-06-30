b767a61edab421b3f82e4824c063e13461033c64
Aleksandr-Bednichenko <a0933093518@gmail.com>
2021-06-30 15:03:31 +0300

    Оформляет модалку для авторизации

--- src/JS/modalSign.js
+++ src/JS/modalSign.js
@@ -3,6 +3,9 @@ import getRefs from './getRefs';
 const refs = getRefs();
 refs.btnSign.addEventListener('click', openModalSign)
 refs.btnCloseSign.addEventListener('click', closeByBtn)
+refs.btnSubmit.addEventListener('click', closeByBtn)
+refs.btnRegister.addEventListener('click', openFormRegister)
+refs.btnCreateAccount.addEventListener('click', createAccount)
 
 function openModalSign() {
     refs.backdropSign.classList.remove('is-hidden')
@@ -14,3 +17,14 @@ function closeByBtn() {
   modal.classList.add('is-hidden');
   bodyEl.classList.remove('remove-scroll');
 }
+
+function openFormRegister() {
+  refs.formAuthorization.classList.add('is-hidden');
+  refs.formRegister.classList.remove('is-hidden')
+}
+
+function createAccount() {
+  refs.formAuthorization.classList.remove('is-hidden');
+  refs.formRegister.classList.add('is-hidden')
+}
+
